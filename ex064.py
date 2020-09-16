da6ea6d2b6d7b9df4947cd870e4614b8996cb7ed
paulamachadomt <paulamachadomt@gmail.com>
2020-09-16 18:09:22 -0300

    update file name

--- ex064.py
+++ ex064.py
@@ -1,11 +1,12 @@
-#utilizando uma flag
+# utilizando uma flag
 flag = 999
-soma = 0
-contador = 0
+soma = 0 
+contador = 0 
 n = int(input('digite um número [999 para finalizar]: '))
-while n != flag:
-    soma += n 
+while n != flag: 
+    soma += n  
     contador += 1
     n = int(input('digite um número [999 para finalizar]: '))   
 print('Você digitou {} números e a soma entre eles foi {}'.format(contador, soma))
 
+ 
